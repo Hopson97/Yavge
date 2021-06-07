6caeb136e29e4200a42a48a8753d9c15a1016272
Matthew Hopson <mhopson@hotmail.co.uk>
2021-06-07 15:57:01 +0100

    Remove comments in main

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ int main()
 {
     bool isMouseActive = true;
 
-    // Init Window, OpenGL set up etc
     sf::Window window;
     if (!initWindow(&window)) {
         return 1;
@@ -16,18 +15,9 @@ int main()
     window.setFramerateLimit(60);
     guiInit(window);
 
-    // Final render target
-    // VertexArray screen;
-    // Shader screenShader;
-    // Framebuffer framebuffer;
-
-    // screenShader.loadFromFile("ScreenVertex.glsl", "ScreenFragment.glsl");
-    // framebuffer.create(WIDTH, HEIGHT);
-    // const Texture2D* colour = framebuffer.addTexture();
-    // framebuffer.finish();
-
-    // The game/ system itself
     Game game;
+
+
 
     Keyboard keyboard;
     while (window.isOpen()) {
@@ -47,28 +37,11 @@ int main()
             }
         }
 
-        // Input + update
         game.onInput(keyboard, window, isMouseActive);
-
         game.onUpdate();
 
-        //
         glEnable(GL_DEPTH_TEST);
-        //   framebuffer.bind();
-        // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Render the regular scene
-        setClearColour(COLOUR_SKY_BLUE);
-        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
         game.onRender();
-
-        // Render the frame buffer content to the window
-        // Framebuffer::unbind();
-        // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        // screenShader.bind();
-        //  colour->bind();
-        //  screen.getRendable().drawArrays(6);
-
         game.onGUI();
         guiEndFrame();
 
